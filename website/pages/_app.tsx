0b5f927b7272ece12d40eee83672c5c37a166d29
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-06-15 15:11:20 -0400

    Added progress bar to indicate page loads

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,21 +1,45 @@
 import '../styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
+
 import type { AppProps } from 'next/app';
-
 import smoothScroll from 'smoothscroll-polyfill';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
+import NextNProgress from 'nextjs-progressbar';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     smoothScroll.polyfill();
   });
+
+  const [isLoading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Handle route change
+    router.events.on('routeChangeStart', (url) => {
+      setLoading(true);
+    });
+
+    router.events.on('routeChangeError', () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    });
+
+    router.events.on('routeChangeComplete', () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -28,6 +52,22 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <DefaultSeo {...SEO} />
+      {isLoading ? (
+        <NextNProgress
+          color="linear-gradient(90deg, #22D3EE, #FACC15)"
+          height="4"
+          transformCSS={(css: string) => {
+            return (
+              <style>
+                {css +
+                  ' #nprogress { position: absolute; top: 4.5rem; z-index: 9999; width: 100vw; }'}
+              </style>
+            );
+          }}
+        />
+      ) : (
+        <></>
+      )}
       <Component {...pageProps} />
     </>
   );
