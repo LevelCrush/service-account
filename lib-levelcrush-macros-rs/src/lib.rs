9bc981baed1daaa1bd2fae7bbab1fde1ebe1a0b1
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-07-28 03:34:10 -0400

    Simplified proc macro choices

--- lib-levelcrush-macros-rs/src/lib.rs
+++ lib-levelcrush-macros-rs/src/lib.rs
@@ -32,76 +32,7 @@ pub fn DatabaseRecord(_attr: TokenStream, item: TokenStream) -> TokenStream {
     }
 
     quote! {
-        #[derive(sqlx::FromRow, Debug, Default, Clone)]
-        #item_struct
-    }
-    .into()
-}
-
-/// appends standard fields related to a record
-#[proc_macro_attribute]
-#[allow(non_snake_case)]
-pub fn DatabaseRecordSerde(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut item_struct = parse_macro_input!(item as ItemStruct);
-
-    if let syn::Fields::Named(ref mut fields) = item_struct.fields {
-        // created_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub created_at: u64 }).unwrap());
-
-        // updated_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub updated_at: u64 }).unwrap());
-
-        // deleted_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub deleted_at: u64 }).unwrap());
-
-        // deleted_at field
-        fields
-            .named
-            .insert(0, syn::Field::parse_named.parse2(quote! { pub id: i64 }).unwrap());
-    }
-
-    quote! {
         #[derive(sqlx::FromRow, Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
-        #item_struct
-    }
-    .into()
-}
-
-/// appends standard fields related to a record
-#[proc_macro_attribute]
-#[allow(non_snake_case)]
-pub fn TimestampFields(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mut item_struct = parse_macro_input!(item as ItemStruct);
-
-    if let syn::Fields::Named(ref mut fields) = item_struct.fields {
-        // created_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub created_at: u64 }).unwrap());
-
-        // updated_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub updated_at: u64 }).unwrap());
-
-        // deleted_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub deleted_at: u64 }).unwrap());
-
-        // deleted_at field
-        fields
-            .named
-            .push(syn::Field::parse_named.parse2(quote! { pub id: i64 }).unwrap());
-    }
-
-    quote! {
         #item_struct
     }
     .into()
@@ -110,19 +41,6 @@ pub fn TimestampFields(_attr: TokenStream, item: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 #[allow(non_snake_case)]
 pub fn DatabaseResult(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let input: proc_macro2::TokenStream = input.into();
-
-    let output = quote! {
-        #[derive(sqlx::FromRow, Debug, Default, Clone)]
-        #input
-    };
-
-    output.into()
-}
-
-#[proc_macro_attribute]
-#[allow(non_snake_case)]
-pub fn DatabaseResultSerde(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input: proc_macro2::TokenStream = input.into();
 
     let output = quote! {
