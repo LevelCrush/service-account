337d5ad40928ef1829b13b6f53d7c9fc581dcd93
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-07-31 04:49:38 -0400

    Migrated service-accounts to same style for running jobs as service-destiny

---
+++ service-accounts/schema/purge.sql
@@ -0,0 +1,6 @@
+drop table if exists  account_platform_data;
+
+drop table if exists  account_platforms;
+
+drop table if exists accounts;
+
---
+++ service-accounts/src/jobs.rs
@@ -0,0 +1,5 @@
+
+pub mod server;
+pub mod discord;
+pub mod purge;
+pub mod reset;
