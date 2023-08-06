ea1e0783c0da7542fae0a51c459be68501c6431f
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-08-06 18:20:46 -0400

    Removed unused bot-destiny and bot-account

--- bot-accounts/src/commands/base_command.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder } from 'discord.js';
-
-export interface Command {
-    name: string;
-    data: SlashCommandBuilder;
-    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-}
