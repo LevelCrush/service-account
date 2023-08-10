60cbbe83e26c58a67de228bd2748993b3f2e4204
Gabriele Nunez <gabrielenunez@protonmail.com>
2023-08-09 22:46:16 -0400

    Moved menu to nextui nav menu

--- app-destiny/src/App.tsx
+++ app-destiny/src/App.tsx
@@ -9,37 +9,49 @@ import {
 import Routes from '@config/routes';
 
 import './App.css';
+import {
+  Navbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from '@nextui-org/react';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <HashRouter>
-      <div className="flex">
-        <nav>
-          <ul>
-            {Routes.map((route, routeIndex) => (
-              <li
-                key={'application_menu_route_' + routeIndex + '_' + route.url}
-              >
-                <Link to={route.url}>{route.name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <main>
-          <ReactRouterRoutes>
-            {Routes.map((route, routeIndex) => (
-              <Route
-                index={routeIndex === 0}
-                path={route.url}
-                key={'application_route_' + routeIndex + '_' + route.url}
-                Component={route.component}
-              />
-            ))}
-          </ReactRouterRoutes>
-        </main>
-      </div>
+      <Navbar
+        onMenuOpenChange={(isOpen) => setIsMenuOpen(isOpen ? true : false)}
+      >
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+          />
+        </NavbarContent>
+        <NavbarMenu>
+          {Routes.map((route, routeIndex) => (
+            <NavbarMenuItem
+              key={`application_menu_route_${routeIndex}_${route.url}`}
+            >
+              <Link to={route.url}>{route.name}</Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+      <main>
+        <ReactRouterRoutes>
+          {Routes.map((route, routeIndex) => (
+            <Route
+              index={routeIndex === 0}
+              path={route.url}
+              key={`application_route_${routeIndex}_${route.url}`}
+              Component={route.component}
+            />
+          ))}
+        </ReactRouterRoutes>
+      </main>
     </HashRouter>
   );
 }
--- app-destiny/src/main.tsx
+++ app-destiny/src/main.tsx
@@ -7,7 +7,9 @@ import { NextUIProvider } from '@nextui-org/react';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <main className="dark text-foreground bg-background">
+        <App />
+      </main>
     </NextUIProvider>
   </React.StrictMode>
 );
